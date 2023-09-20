a3ccd55a27d9c8304174aee9631bdc27f851fce5
Rubanov <bykr3m@gmail.com>
2023-09-20 18:07:05 +0300

    Add RelayCommand.cs and ObservableObject.cs

---
+++ GIModMan/Core/ObservableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GIModMan.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+        protected bool Set<T>(ref T field, T newVal, [CallerMemberName] string propName = null)
+        {
+            if(Equals(field, newVal))
+            {
+                return false;
+            }
+
+            field = newVal;
+            OnPropertyChanged(propName);
+            return true;
+        }
+    }
+}
